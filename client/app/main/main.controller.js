1072afdd801679101d128ce86d609be78b909f87
opankov <pankov.oleksandr@gmail.com>
2016-05-05 17:42:45 +0300

    created TRs in view with buttons

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,26 +6,28 @@ class MainController {
 
   constructor(transferDataService, $scope) {
 
-    this.services = [];
+    this.servers = [];
     this.transferDataService = transferDataService();
+    this.test = "test";
   }
 
   $onInit() {
-    // this.$http.get('/api/things').then(response => {
-    //   this.awesomeThings = response.data;
-    //   console.log("this.things", this.awesomeThings);
-    // });
+
     console.log("INIT CONTROLLER");
-    this.services = this.transferDataService.getServisesList();
-    console.log('awesomeThings', this.services);
+    this.servers = this.transferDataService.getServisesList();
+    console.log('this', this);
 
   }
 }
+//   function MainController() {
+//     this.test = "test";
+//   }
 
 angular.module('videoServersApp')
   .component('main', {
     templateUrl: 'app/main/main.html',
-    controller: MainController
+    controller: MainController,
+    controllerAs: 'main'
   });
 
 })();
