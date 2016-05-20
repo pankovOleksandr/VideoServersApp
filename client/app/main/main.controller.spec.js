310d6064ff1912cac7d2ad7905100866afd1d4b4
opankov <pankov.oleksandr@gmail.com>
2016-05-20 17:26:53 +0300

    started tests for transferDataSrv

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -7,28 +7,46 @@ describe('Component: mainComponent', function() {
 
   var scope;
   var mainComponent;
-  var $httpBackend;
+  var mockTransferDataFactory = {
+      getServersList: function () {
+        return [{
+          "ip": "10.47.108.594",
+          "name": "ATOMICA",
+          "currentVersion": "336.25.80.52"
+        },
+          {
+            "ip": "10.47.108.766",
+            "name": "OPTYK",
+            "currentVersion": "941.52.80.21"
+          }]
+      },
+      getVersions: function () {
+        return ["111.93.55.31", "351.22.82.91", "981.55.10.23"];
+      }
+    }
+
+
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function(
-    _$httpBackend_,
-    $http,
     $componentController,
     $rootScope) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('/api/things')
-        .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+      // $httpBackend = _$httpBackend_;
+      // $httpBackend.expectGET('/api/things')
+      //   .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
       scope = $rootScope.$new();
+
       mainComponent = $componentController('main', {
-        $http: $http,
+        transferDataFactory: mockTransferDataFactory,
         $scope: scope
       });
   }));
 
-  // it('should attach a list of things to the controller', function() {
-  //   mainComponent.$onInit();
-  //   $httpBackend.flush();
-  //   expect(mainComponent.awesomeThings.length).toBe(4);
-  // });
+  it('should attach a list of things to the controller', function() {
+    mainComponent.$onInit();
+    // $httpBackend.flush();
+    expect(mainComponent.servers.length).toBe(2);
+    expect(mainComponent.versions.length).toBe(3);
+  });
 });
--- client/app/sharedServices/transferData.service.spec.js
+++ client/app/sharedServices/transferData.service.spec.js
@@ -1,3 +1,63 @@
 /**
  * Created by opankov on 5/19/2016.
  */
+'use strict';
+
+describe('Service: transferData', function(){
+  var defaultData,
+      localStorage,
+      transferData;
+
+  beforeEach(function() {
+
+    module('videoServersApp');
+
+    module(function($provide) {
+      $provide.factory('defaultData', function() {
+       
+        return {
+          getServers: function () {
+            return [{
+              "ip": "10.47.108.594",
+              "name": "ATOMICA",
+              "currentVersion": "336.25.80.52"
+            },
+              {
+                "ip": "10.47.108.766",
+                "name": "OPTYK",
+                "currentVersion": "941.52.80.21"
+              }]
+          },
+          getVersions: function () {
+            return ["111.93.55.31", "351.22.82.91", "981.55.10.23"];
+          }
+        }
+      });
+
+      $provide.provider('localStorageService', function() {
+        this.$get = function() {
+          var setItem = jasmine.createSpy('setItem'),
+              getItem = jasmine.createSpy('getItem');
+          return {
+            saveToLocalStorage : setItem,
+            getFromLocalStorage : getItem
+          }
+        }
+      })
+    });
+
+    inject(function(_defaultData_, _localStorageService_, _transferDataFactory_ ) {
+      defaultData = _defaultData_;
+      localStorage = _localStorageService_;
+      transferData = _transferDataFactory_;
+    });
+  });
+
+  it('should TRANSFER DATA', function() {
+    console.log('TRANSFER DATA', transferData);
+
+
+    var ser = transferData.getServersList();
+    console.log('SER', ser);
+  });
+});
